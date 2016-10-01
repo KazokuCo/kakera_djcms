d8dc15006af585486cc3417447e01246339a21b7
uppfinnarn <uppfinnarn@gmail.com>
2016-10-01 15:20:52 +0200

    working newsblog

--- kakera/settings.py
+++ kakera/settings.py
@@ -54,6 +54,17 @@ INSTALLED_APPS = [
     'djangocms_markdown',
     'djangocms_text_ckeditor',
 
+    'aldryn_apphooks_config',
+    'aldryn_categories',
+    'aldryn_common',
+    'aldryn_newsblog',
+    'aldryn_people',
+    'aldryn_reversion',
+    'aldryn_translation_tools',
+    'parler',
+    'sortedm2m',
+    'taggit',
+
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
